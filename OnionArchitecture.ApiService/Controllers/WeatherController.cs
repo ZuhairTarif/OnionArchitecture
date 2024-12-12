8e35bfe58d0dfe11d977f4ec1f7740c6d6dfe579
ZuhairTarif <20-42115-1@student.aiub.edu>
2024-12-12 17:49:49 +0600

    api processing

---
+++ OnionArchitecture.ApiService/Controllers/WeatherController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.ApiService.Models;
+
+namespace OnionArchitecture.ApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("get")]
+        public WeatherForecast Get()
+        {
+            return new WeatherForecast
+            {
+                Date = new DateOnly(2021, 1, 1),
+                TemperatureC = 25,
+                Summary = "Sunny"
+            };
+        }
+    }
+}
---
+++ OnionArchitecture.ApiService/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace OnionArchitecture.ApiService.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .WithMethods("POST", "GET", "PUT")
+                        .AllowAnyHeader());
+            });
+    }
+}
